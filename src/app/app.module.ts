2b1a09b9b68ebc5eded8ddc8cf1d9e78cf98026e
KatiaCursi <cursikatia@gmail.com>
2024-10-20 20:25:26 -0300

    criação do componente tabela livros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TabelaLivrosComponent } from './tabela-livros/tabela-livros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabelaLivrosComponent
   ],
   imports: [
     BrowserModule,
